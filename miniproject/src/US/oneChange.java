440a19c79f8de99c59e3a50e8d8bcb5dcfc391b2
Durgendra Nath Upadhyay <durgendra12345@gmail.com>
2020-12-18 12:07:57 +0530

    6th commit

--- miniproject/src/US/oneChange.java
+++ miniproject/src/US/oneChange.java
@@ -4,7 +4,7 @@ public class oneChange {
 
 	public static void main(String[] args) {
 			
-		System.out.println("New Change 2");
+		System.out.println("New Change 2 ");
 		System.out.println("Another Change 2");
 		System.out.println("Another Change 3");
 	}
